5c7ea837293d1e6a8e6205e59bc7e6f3f72be0ab
Mateus Kern <mateus.g.kern@gmail.com>
2022-06-24 14:32:28 -0300

    Iniciando um validador de listas de comandos

--- PetShop.Domain/Utils/Extensions.cs
+++ PetShop.Domain/Utils/Extensions.cs
@@ -1,4 +1,7 @@
-﻿public static class Extensions
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+public static class Extensions
 {
     /// <summary>
     /// Returns string without special characters
@@ -12,4 +15,19 @@
 
     public static int MaximumNumberPossible(int precision, int scale) =>
         Convert.ToInt32("9".PadRight(precision - scale,'9'));
+
+    public static Contract<Notification> ValidateListCommand(List<Command> listCommand, string key)
+    {
+        var contract = new Contract<Notification>();
+
+        if (listCommand is not null)
+            for (int i = 0; i < listCommand.Count; i++)
+            {
+                listCommand[i].Validate();
+                foreach (var notification in listCommand[i].Notifications)
+                    contract.AddNotification($"{key}.{i}.{notification.Key}", notification.Message);
+            }
+
+        return contract;
+    }
 }
